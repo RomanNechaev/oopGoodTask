c9df8b4a522587cf8b8550294cd32ed54d5ead15
RomanNechaev <romenkagg@gmail.com>
2023-04-16 13:16:30 +0500

    отредактировал код под требования задачи

--- src/main/java/ru/oop/Car.java
+++ src/main/java/ru/oop/Car.java
@@ -4,16 +4,10 @@ package ru.oop;
  * @author Nechaev Roman
  */
 public class Car {
-    private final Person person;
-    private Position position;
 
     Car(Person person) {
-        this.person = person;
-        position = person.getPosition();
-    }
 
+    }
     public void go(Position destination) {
-        person.setPosition(destination);
-        position = destination;
     }
 }
